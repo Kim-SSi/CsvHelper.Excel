fa407fadf53f11cd08855e126f7fa909202be809
Kim <kim@ssi.co.nz>
2024-09-19 15:34:55 +1200

    Add constructor to correct for codacy warning

--- src/CsvHelper.Excel/ExcelParser.cs
+++ src/CsvHelper.Excel/ExcelParser.cs
@@ -104,6 +104,16 @@ namespace CsvHelper.Excel
         /// <param name="stream">The stream.</param>
         /// <param name="sheetName">The sheet name</param>
         /// <param name="configuration">The configuration.</param>
+        public ExcelParser(Stream stream, string sheetName, CsvConfiguration configuration) : this(stream, sheetName, configuration, false)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExcelParser"/> class.
+        /// </summary>
+        /// <param name="stream">The stream.</param>
+        /// <param name="sheetName">The sheet name</param>
+        /// <param name="configuration">The configuration.</param>
         /// <param name="leaveOpen"><c>true</c> to leave the <see cref="TextWriter"/> open after the <see cref="ExcelParser"/> object is disposed, otherwise <c>false</c>.</param>
         public ExcelParser(Stream stream, string sheetName, CsvConfiguration configuration, bool leaveOpen = false)
         {
